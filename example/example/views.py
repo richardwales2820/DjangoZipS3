a82c3e9377bf0632bef4a089ca9e7bd74b0a1968
Richard Wales <richard_wales@knights.ucf.edu>
2017-05-28 00:28:40 -0700

    Refactored, add to readme

--- example/example/views.py
+++ example/example/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render, redirect
 import django_zips3
 
 def index(request):
-    download = django_zips3.generate_zips3("/Nothing/POS2041")    
+    download = django_zips3.generate_url("/Nothing/POS2041")    
     return redirect(download)
     return render(request, "example/index.html", {})
