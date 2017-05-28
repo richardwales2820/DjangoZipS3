c152c0627858ade46a43dda4d7fa1e1c39bc2bb4
Richard Wales <richard_wales@knights.ucf.edu>
2017-05-28 02:17:22 -0700

    Beautified demo and fixed encoding bug

--- example/example/views.py
+++ example/example/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, redirect
+from django.http import Http404
 import django_zips3
 
 def index(request):
-    download = django_zips3.generate_url("/Nothing/POS2041")    
-    return redirect(download)
+    # download = django_zips3.generate_url("/Nothing/POS2041")    
+    # return redirect(download)
     return render(request, "example/index.html", {})
+
+def download(request):
+    if request.method != 'POST':
+        return Http404
+    
+    download = django_zips3.generate_url(request.POST.get('prefix'))    
+    return redirect(download)
